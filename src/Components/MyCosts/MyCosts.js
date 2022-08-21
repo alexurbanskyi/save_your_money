517449f273a27ff7a640ee2aea2f54cfc7480f8c
kalitsinskyi <vlad.kalitsinskij@transfotech.com>
2022-08-21 13:35:13 +0300

    icon

--- src/Components/MyCosts/MyCosts.js
+++ src/Components/MyCosts/MyCosts.js
@@ -1,21 +1,27 @@
-import React, { useLayoutEffect, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import {useSelector} from "react-redux";
 import './MyCosts.css'
+import { GiCigarette } from "react-icons/gi";
+import { IoBeer } from "react-icons/io5";
+import { BiCoffeeTogo } from "react-icons/bi";
 
 function MyCosts(props) {
 
     const [period, setPeriod]  = useState(null)
     const [periodCosts, setPeriodCosts] = useState(0)
-    console.log('PERIOD--->',period)
+    const [periodCostsList, setPeriodCostList] = useState(null)
+    // console.log('PERIOD--->',period)
         
     const purchase = useSelector(state => state.purchase)
-    console.log('purchase ----->',purchase)
+    // console.log('purchase ----->',purchase)
 
     const currentDate = (new Date()).toLocaleDateString().split('.')
-    console.log('current date --->', currentDate)
+    // console.log('current date --->', currentDate)
 
    useLayoutEffect(() =>{
         const dayCostsList = purchase.filter((elem) => elem.day === currentDate[0])
+        console.log('dayCostsList ---->', dayCostsList)
+        setPeriodCostList(dayCostsList);
         setPeriod('- TODAY -')
         const dayCosts = dayCostsList.reduce((acc, currentValue) => { 
             return(acc + currentValue.price*1)
@@ -23,6 +29,8 @@ function MyCosts(props) {
         setPeriodCosts(dayCosts)
    },[]); 
    
+   console.log('periodCostsList --->', periodCostsList)
+
 
    const todayCosts = () => {
         const dayCostsList = purchase.filter((elem) => elem.day === currentDate[0])
@@ -40,6 +48,14 @@ function MyCosts(props) {
         setPeriod('- ALL TIME -')
         setPeriodCosts(allTimeCosts)
    }
+//    const categoryList = {
+//     shop: ['food', 'clothing', 'cleanliness'],
+//     hobby: ['fishing', 'gym'],
+//     fun: ['cafe', 'birthday'],
+//     transport: ['ticket'],
+//     car:['fuel', 'repair'],
+//     "bad habits": ['beer', 'coffee', 'cigarettes']
+// }
    
     
   
@@ -56,6 +72,22 @@ function MyCosts(props) {
                 <button className='btn-period'>chosen date</button>
                 <button className='btn-period'>month</button>
                 <button className='btn-period' onClick={allTime}>all time</button>
+            </div>
+            <div className='costs-category'>
+                <div className='category-title'>bad habits</div>
+                <div className='type-holder'>
+                    <div>
+                        <GiCigarette className='type-icon'/>
+                        <p>123</p>
+                    </div>
+                    <div>
+                        <IoBeer className='type-icon'/>
+                    </div>
+                    <div>
+                        <BiCoffeeTogo className='type-icon' />
+                    </div>
+                </div>
+                
             </div>
             
            
